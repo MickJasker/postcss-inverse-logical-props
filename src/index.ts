2186349e6dc52b2fad17ae1c18243ea420e97e02
Mick Jasker <mick.jasker@mediamonks.com>
2023-08-07 10:51:18 +0200

    Add debug console

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ const postCssInverseLogicalProperties: postcss.PluginCreator<undefined> = () => 
   postcssPlugin: 'postcss-inverse-logical-properties',
   // eslint-disable-next-line @typescript-eslint/naming-convention
   Declaration(decl): void {
+    console.debug('postcss-inverse-logical-properties: Declaration', decl.prop);
+
     if (decl.prop === 'left') {
       decl.replaceWith(decl.clone({ prop: 'inset-inline-start' }));
     }
